fdd0ff20690360bb7ecdab0e2736b18e273b19b2
Fabián E García <garciafabianenrique79@gmail.com>
2025-09-18 14:59:33 -0300

    Script del profe
    Terminé de ver la clase de networking, seguí los pasos y está todo funcional hasta el momento.
    PD: se testeó spawneen en la escena de juego, no se testeó controles input.

---
+++ Assets/_Proyect/Scripts/NetworkControllerNico.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Fusion;
+using Fusion.Sockets;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+
+public class NetworkControllerNico : MonoBehaviour, INetworkRunnerCallbacks
+{
+    [Header("UI Elements")]
+    [SerializeField] private GameObject _lobbyPanel;
+    [SerializeField] private Button _createRoomButton;
+    [SerializeField] private Button _joinRoomButton;
+
+    [Header("Network Components")]
+    [SerializeField] private NetworkRunner _networkRunner;
+    [SerializeField] private NetworkSceneManagerDefault _networkSceneManagerDefault;
+
+    [SerializeField] private NetworkObject _playerPrefab;
+
+    private void Start()
+    {
+        _createRoomButton.onClick.AddListener(CreateRoom);
+        _joinRoomButton.onClick.AddListener(JoinRoom);
+    }
+
+    private async void CreateRoom()
+    {
+        var gameArg = new StartGameArgs()
+        {
+            GameMode = GameMode.Host,
+            SessionName = "Room_1",
+            SceneManager = _networkSceneManagerDefault,
+        };
+
+        var result = await _networkRunner.StartGame(gameArg);
+
+        if (!result.Ok)
+        {
+            Debug.Log(result.ShutdownReason);
+        }
+    }
+
+    private async void JoinRoom()
+    {
+        var gameArg = new StartGameArgs()
+        {
+            GameMode = GameMode.Client,
+            SessionName = "Room_1",
+            SceneManager = _networkSceneManagerDefault,
+        };
+
+        var result = await _networkRunner.StartGame(gameArg);
+
+        if (!result.Ok)
+        {
+            Debug.Log(result.ShutdownReason);
+        }
+    }
+
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log("OnPlayerJoined");
+        _lobbyPanel.SetActive(false);
+
+        if (!_networkRunner.IsServer) return;
+
+        _networkRunner.Spawn(_playerPrefab, new Vector3(Random.Range(-3,3), 0, 0), Quaternion.identity, player);
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+    }
+
+
+
+    //========================================================\\
+
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+    }
+
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+    }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+    }
+
+   
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+    }
+}
