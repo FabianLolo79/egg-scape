d436e235cc63feb72ff1566ce5ef436f0dd8362f
Fabián E García <garciafabianenrique79@gmail.com>
2025-10-02 15:34:25 -0300

    Clase Network pt4
    emote Procedural Calls (RPC)

--- Assets/_Proyect/Scripts/Player/PlayerHealth.cs
+++ Assets/_Proyect/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 using Fusion;
+using System.Collections.Generic;
+using System.Collections;
 
 public class PlayerHealth : NetworkBehaviour
 {
-    [Networked, OnChangedRender(nameof(OnHealthChanged))] public int health { get; set; } = 100;
+    [Networked] public int health { get; set; } = 100;
+
+    [SerializeField] private MeshRenderer _renderer;
+    private ChangeDetector _changeDetector;
+
+    public override void Spawned()
+    {
+        _changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState);
+    }
 
     public override void FixedUpdateNetwork()
     {
@@ -11,20 +21,55 @@ public class PlayerHealth : NetworkBehaviour
         {
             if (inputPlayer.buttons.IsSet(NetworkInputPlayer.MOUSE_BUTTON_1))
             {
-                TakeDamage(10);
+                RPC_RequestDamage(10);
             }
         }
     }
 
-    public void TakeDamage(int damage)
+    public override void Render()
+    {
+        foreach (var changeVariable in _changeDetector.DetectChanges(this))
+        {
+            if (changeVariable == nameof(health))
+            {
+                Debug.Log($"Health changed to {health}");
+            }
+        }
+    }
+
+    //Server Logic
+    private void ServerTakeDamage(int damage)
     {
         if(!HasStateAuthority) return;
 
-        health -= damage;
+        health = Mathf.Max(0, health - damage);
     }
 
-    private void OnHealthChanged()
+    
+    // Client (InputAuthority) --> Server
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    private void RPC_RequestDamage(int damage, RpcInfo info = default)
     {
-        Debug.Log($"Has State Auth: [{HasStateAuthority}]. Has Input Auth: [{HasInputAuthority}]. OnHealthChanged: [{health}]");
-    }  
+        if (!HasInputAuthority) return;
+
+        ServerTakeDamage(damage);
+
+        RPC_HitDamageFeedback();
+    }
+
+    //Server --> Clients (Alls)
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All, Channel = RpcChannel.Unreliable)]
+    private void RPC_HitDamageFeedback(RpcInfo info = default)
+    {
+        StartCoroutine(DamageFeedBack());
+    }
+
+    private IEnumerator DamageFeedBack()
+    {
+        var initialColor = _renderer.material.color;
+        _renderer.material.color = Color.yellow;
+        yield return new WaitForSeconds(0.3f);
+        _renderer.material.color = initialColor;
+    }
+
 }
